38b3586d9f4788c95c076c7d21873b1584c38de6
Varghese Paul <varkychen@gmail.com>
2013-06-19 06:56:13 +0530

    Changes to compile code

--- src/main/java/delta/manager/main/DeltaManagerMain.java
+++ src/main/java/delta/manager/main/DeltaManagerMain.java
@@ -1,13 +1,12 @@
 package delta.manager.main;
 
 import delta.manager.mvc.MVC;
-import delta.manager.mvc.model.TradeData;
 import delta.manager.utils.BeanDispenser;
 
 public class DeltaManagerMain {
 
 	public static void main(String[] args) {
-		MVC<TradeData> deltaManager = BeanDispenser.dispenseBean("delta.manager.main.beans.xml", "mvc", MVC<TradeData>.class)
+		MVC<?> deltaManager = BeanDispenser.dispenseBean("delta.manager.main.beans.xml", "mvc",MVC.class);
 		deltaManager.startMVC();
 	}
 }
